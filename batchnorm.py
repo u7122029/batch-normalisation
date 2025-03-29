Fixed running variance and added 3D functionality in 1d batchnorm.

batchnorm.py
@@ -16,18 +16,38 @@ class MyBatchNorm1d(nn.Module):
 
     def forward(self, x):
         if self.training:
-            batch_mean = x.mean(dim=0)
-            batch_var = x.var(dim=0, correction=0)
+            if len(x.shape) == 2:
+                batch_mean = x.mean(dim=0)
+                batch_var = x.var(dim=0, correction=0)
+            elif len(x.shape) == 3:
+                batch_mean = x.mean(dim=(0, 2))
+                batch_var = x.var(dim=(0, 2), correction=0)
+            else:
+                raise Exception("Invalid dimension")
 
             with torch.no_grad():
+                m = x.numel() / x.size(1)
                 self.running_mean = (1 - self.momentum) * self.running_mean + self.momentum * batch_mean.squeeze()
-                self.running_var  = (1 - self.momentum) * self.running_var + self.momentum * batch_var.squeeze()
-
-            normalised = (x - batch_mean) / torch.sqrt(batch_var + self.eps)
+                self.running_var  = (1 - self.momentum) * self.running_var + self.momentum * batch_var.squeeze() * m / (m - 1)
+            if len(x.shape) == 2:
+                normalised = (x - batch_mean) / torch.sqrt(batch_var + self.eps)
+            elif len(x.shape) == 3:
+                normalised = (x - batch_mean.view(1, -1, 1)) / torch.sqrt(batch_var.view(1, -1, 1) + self.eps)
+            else:
+                raise Exception("You shouldn't be here!")
         else:
-            normalised = (x - self.running_mean) / torch.sqrt(self.running_var + self.eps)
+            if len(x.shape) == 2:
+                normalised = (x - self.running_mean) / torch.sqrt(self.running_var + self.eps)
+            elif len(x.shape) == 3:
+                normalised = (x - self.running_mean.view(1, -1, 1)) / torch.sqrt(self.running_var.view(1, -1, 1) + self.eps)
+            else:
+                raise Exception("You shouldn't be here!")
 
-        return normalised * self.weight + self.bias
+        if len(x.shape) == 2:
+            return normalised * self.weight + self.bias
+        else:
+            return normalised * self.weight.view(1,-1,1) + self.bias.view(1,-1,1)
+
 
 class MyBatchNorm2d(nn.Module):
     def __init__(self, features, eps=1e-5, momentum=0.1):
@@ -48,14 +68,13 @@ class MyBatchNorm2d(nn.Module):
             batch_var = x.var(dim=(0, 2, 3), correction=0, keepdim=True)
 
             with torch.no_grad():
+                m = x.numel() / x.size(1)
                 self.running_mean = (1 - self.momentum) * self.running_mean + self.momentum * batch_mean.squeeze()
-                self.running_var = (1 - self.momentum) * self.running_var + self.momentum * batch_var.squeeze()
+                self.running_var = (1 - self.momentum) * self.running_var + self.momentum * batch_var.squeeze() * m / (m - 1)
 
             normalised = (x - batch_mean) / torch.sqrt(batch_var + self.eps)
         else:
-            print(self.running_mean.shape, self.running_var.shape)
             normalised = (x - self.running_mean.view(1, -1, 1, 1)) / torch.sqrt(self.running_var.view(1, -1, 1, 1) + self.eps)
 
         out = self.weight.view(1,-1,1,1) * normalised + self.bias.view(1,-1,1,1)
         return out
-

tests.py
@@ -5,19 +5,20 @@ from batchnorm import MyBatchNorm1d, MyBatchNorm2d
 
 
 class MyTestCase(unittest.TestCase):
-    def test_bn1d(self):
+    def test_bn1d_1d(self):
         batch_norm_torch = nn.BatchNorm1d(50).double()
         batch_norm_mine = MyBatchNorm1d(50).double()
 
         batch_size = 16
 
+        # Tests during training mode
         for i in range(10):
             inp_tensor = (torch.rand(batch_size, 50) * torch.rand(batch_size, 50) * 5 + torch.rand(batch_size, 50) * 10).double()
 
             left = batch_norm_torch(inp_tensor)
             right = batch_norm_mine(inp_tensor)
 
-            self.assertTrue(torch.isclose(torch.norm(left - right), torch.tensor(0.0).double()))
+            self.assertTrue(torch.isclose(torch.norm(left - right), torch.tensor(0.0).double(), atol=1e-6)) # all of these pass
 
         batch_norm_mine.eval()
         batch_norm_torch.eval()
@@ -25,8 +26,34 @@ class MyTestCase(unittest.TestCase):
         inp_tensor = (torch.rand(batch_size, 50) * torch.rand(batch_size, 50) * 5 + torch.rand(batch_size, 50) * 10).double()
         left = batch_norm_torch(inp_tensor)
         right = batch_norm_mine(inp_tensor)
-        # print(torch.norm(left - right))
-        self.assertTrue(torch.isclose(torch.norm(left - right), torch.tensor(0.0).double())) # assertion error here for some reason. Maybe it's implementation differences, or just precision.
+        self.assertTrue(torch.isclose(torch.norm(left - right), torch.tensor(0.0).double(), atol=1e-6)) # assertion error here for some reason. Maybe it's implementation differences, or just precision.
+
+    def test_bn1d_2d(self):
+        batch_norm_torch = nn.BatchNorm1d(50).double()
+        batch_norm_mine = MyBatchNorm1d(50).double()
+
+        batch_size = 16
+        features = 50
+        seq_length = 100
+
+        # Tests during training mode
+        for i in range(10):
+            inp_tensor = (torch.rand(batch_size, features, seq_length) * torch.rand(batch_size, features, seq_length) * 5 + torch.rand(batch_size, features, seq_length) * 10).double()
+
+            left = batch_norm_torch(inp_tensor)
+            right = batch_norm_mine(inp_tensor)
+
+            self.assertTrue(torch.isclose(torch.norm(left - right), torch.tensor(0.0).double(), atol=1e-6)) # all of these pass
+
+        batch_norm_mine.eval()
+        batch_norm_torch.eval()
+
+        inp_tensor = (torch.rand(batch_size, features, seq_length) * torch.rand(batch_size, features, seq_length) * 5 + torch.rand(batch_size, features, seq_length) * 10).double()
+
+        left = batch_norm_torch(inp_tensor)
+        right = batch_norm_mine(inp_tensor)
+        self.assertTrue(torch.isclose(torch.norm(left - right), torch.tensor(0.0).double(), atol=1e-6)) # assertion error here for some reason. Maybe it's implementation differences, or just precision.
+
 
     def test_bn2d(self):
         batch_size = 4
@@ -41,7 +68,7 @@ class MyTestCase(unittest.TestCase):
             inp_tensor = (torch.rand(batch_size, channels, width, height) * torch.rand(batch_size, channels, width, height) * 5 + torch.rand(batch_size, channels, width, height) * 10).double()
             left = batch_norm_torch(inp_tensor)
             right = batch_norm_mine(inp_tensor)
-            self.assertTrue(torch.isclose(torch.norm(left - right), torch.tensor(0.0).double()))
+            self.assertTrue(torch.isclose(torch.norm(left - right), torch.tensor(0.0).double(), atol=1e-6))
 
         batch_norm_mine.eval()
         batch_norm_torch.eval()
@@ -49,8 +76,7 @@ class MyTestCase(unittest.TestCase):
         inp_tensor = (torch.rand(batch_size, channels, width, height) * torch.rand(batch_size, channels, width, height) * 5 + torch.rand(batch_size, channels, width, height) * 10).double()
         left = batch_norm_torch(inp_tensor)
         right = batch_norm_mine(inp_tensor)
-        # print(torch.norm(left - right))
-        self.assertTrue(torch.isclose(torch.norm(left - right), torch.tensor(0.0).double())) # assertion error here for some reason. Maybe it's implementation differences, or just precision.
+        self.assertTrue(torch.isclose(torch.norm(left - right), torch.tensor(0.0).double(), atol=1e-6)) # assertion error here for some reason. Maybe it's implementation differences, or just precision.
 
 if __name__ == '__main__':
     unittest.main()